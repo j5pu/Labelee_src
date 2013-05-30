2cb27db73ae2b0935260582e2344cdf3deb3191b
Rmaja <rmajasol@gmail.com>
2013-05-30 11:00:25 +0200

    Imprimir mapa

--- map/urls.py
+++ map/urls.py
@@ -5,7 +5,7 @@ urlpatterns = patterns('map.views',
     url(r'^your-position/(\d{3})$', 'your_position'),
     url(r'^event-log$', 'event_log'),
 
-    # map/(1_25_91234)*
+    # /map/(1_25_91234)*
     url(r'^((?P<enclosure_id>\d+)_(?P<floor_id>\d+)_(?P<poi_id>\d+))*$', 'origin'),
 
     )
