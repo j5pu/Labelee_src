d1836b7e115ff689016bb4b8a076e89f70c26659
josemnopi <jose.m.duque@mnopi.com>
2013-06-28 13:46:39 +0200

    Social corregido (lupa capada)

--- static-media/js/map/index/social.network.menu.js
+++ static-media/js/map/index/social.network.menu.js
@@ -44,7 +44,9 @@ function get_url() {
     if (prevDest) {
         urlstring += 'dest/' + prevDest.enclosureid + '_' + prevDest.floorid + '_' + prevDest.poid;
         return urlstring;
-    } else {
+    }
+    //CAPADO HASTA QUE ACTIVEMOS LA LUPA DE BUSQUEDA
+    /* else {
         for (var sfl in floors) {
             for (var poi in floors[sfl].pois) {
                 if (searchMarker._markerLoc._circleLoc._latlng == floors[sfl].pois[poi].marker._latlng) {
@@ -53,7 +55,7 @@ function get_url() {
                 }
             }
         }
-    }
+    }*/
 
     return location.href;
 }
