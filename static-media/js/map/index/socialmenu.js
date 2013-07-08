19f7f8f31c51be1fb4cc0fa4ffbde6ccd9a93587
Alvaro Gutierrez Leña <alvaro.gutierrez@mnopi.com>
2013-07-08 17:16:26 +0200

    Arreglado un bug en el Social menu.

--- static-media/js/map/index/socialmenu.js
+++ static-media/js/map/index/socialmenu.js
@@ -6,53 +6,53 @@ var SocialMenu = {
     renderIcon: function (pointId) {
 
 
-        return '<p align="center"><button class="socialmenu" style=" left:0px; margin: 0%;" onclick="SocialMenu.fbs_click('+ pointId+')" data-socialmenu="' + pointId + '">' +
+        return '<p align="center"><button class="socialmenu" style=" left:0px; margin: 0%;" onclick="SocialMenu.fbs_click(' + pointId + ')" data-socialmenu="' + pointId + '">' +
             '<i class="icon-facebook-sign"></i>' +
             '</button>' +
-            '<button class="socialmenu" style=" left:0px; margin: 0%;"  onclick="SocialMenu.twt_click('+ pointId+')" data-socialmenu="' + pointId + '">' +
+            '<button class="socialmenu" style=" left:0px; margin: 0%;"  onclick="SocialMenu.twt_click(' + pointId + ')" data-socialmenu="' + pointId + '">' +
             '<i class="icon-twitter-sign"></i>' +
-            '</button>'+
-              '<button class="socialmenu" style=" left:0px; margin: 0%;" onclick="SocialMenu.google_click('+ pointId+')" data-socialmenu="' + pointId + '">' +
+            '</button>' +
+            '<button class="socialmenu" style=" left:0px; margin: 0%;" onclick="SocialMenu.google_click(' + pointId + ')" data-socialmenu="' + pointId + '">' +
             '<i class="icon-google-plus-sign"></i>' +
             '</button></p>'
 
     },
 
     bindShow: function () {
-       /* $('.leaflet-popup-content button.socialmenu').on('click', function () {
-            // SocialMenu.marker.bindPopup(html).openPopup();
-            if(SocialMenu.opened) SocialMenu.close();
-            SocialMenu.opened = true;
+        /* $('.leaflet-popup-content button.socialmenu').on('click', function () {
+         // SocialMenu.marker.bindPopup(html).openPopup();
+         if(SocialMenu.opened) SocialMenu.close();
+         SocialMenu.opened = true;
 
-            var point_id = $(this).data('socialmenu');
-            SocialMenu.point = new PointResource().read(point_id);
-            SocialMenu.floor = new FloorResource().readFromUri(SocialMenu.point.floor);
-            SocialMenu.enclosure = enclosure_id;
-            SocialMenu.url = SocialMenu.urlstring+ SocialMenu.enclosure+'_'+SocialMenu.floor.id+'_'+SocialMenu.point.id;
-            SocialMenu.urlTitle =  'would you like to go to'+SocialMenu.point.description +'?';
-            var html =  '<span id="social" >';
-            html += '<ul class="navi">';
-            html+= '<li id="n1" class="n1" style="left: 10px"><a class="mjn-tab1" href="#" title="{% trans "Compartir con" %} Facebook" onclick="SocialMenu.fbs_click()"><span> <i class="icon-facebook-sign"></i></span></a> </li>';
-            html+= ' <li id="n2" class="n2" style="left: 50px"><a class="mjn-tab2" href="#" title="{% trans "Compartir con" %} Twitter" onclick="SocialMenu.twt_click()"><span>  <i class="icon-twitter-sign"></i></span></a> </li>';
-            html+='<li id="n3" class="n3"  style="left: 90px"><a class="mjn-tab3" href="#" title="{% trans "Compartir con" %} Google"    onclick="SocialMenu.google_click()"><span>  <i class="icon-google-plus-sign"></i></span></a>  <button id="closeSocial" class="button" onclick="SocialMenu.close()">x</button> </li>'
-            html+='   </li>   </ul>   </span>'
+         var point_id = $(this).data('socialmenu');
+         SocialMenu.point = new PointResource().read(point_id);
+         SocialMenu.floor = new FloorResource().readFromUri(SocialMenu.point.floor);
+         SocialMenu.enclosure = enclosure_id;
+         SocialMenu.url = SocialMenu.urlstring+ SocialMenu.enclosure+'_'+SocialMenu.floor.id+'_'+SocialMenu.point.id;
+         SocialMenu.urlTitle =  'would you like to go to'+SocialMenu.point.description +'?';
+         var html =  '<span id="social" >';
+         html += '<ul class="navi">';
+         html+= '<li id="n1" class="n1" style="left: 10px"><a class="mjn-tab1" href="#" title="{% trans "Compartir con" %} Facebook" onclick="SocialMenu.fbs_click()"><span> <i class="icon-facebook-sign"></i></span></a> </li>';
+         html+= ' <li id="n2" class="n2" style="left: 50px"><a class="mjn-tab2" href="#" title="{% trans "Compartir con" %} Twitter" onclick="SocialMenu.twt_click()"><span>  <i class="icon-twitter-sign"></i></span></a> </li>';
+         html+='<li id="n3" class="n3"  style="left: 90px"><a class="mjn-tab3" href="#" title="{% trans "Compartir con" %} Google"    onclick="SocialMenu.google_click()"><span>  <i class="icon-google-plus-sign"></i></span></a>  <button id="closeSocial" class="button" onclick="SocialMenu.close()">x</button> </li>'
+         html+='   </li>   </ul>   </span>'
 
-            $(this).parent().append(html);
-
-
-
-    /*        $('button#close').css({
+         $(this).parent().append(html);
 
 
-           })
-                .on('click', function () {
-                   SocialMenu.close();
-               jQuery('canvas').remove();
-                jQuery('body').find('.navi').remove();
-              $(this).remove();
-                })
-              ;*/
-      //  });
+
+         /*        $('button#close').css({
+
+
+         })
+         .on('click', function () {
+         SocialMenu.close();
+         jQuery('canvas').remove();
+         jQuery('body').find('.navi').remove();
+         $(this).remove();
+         })
+         ;*/
+        //  });
     },
 
     renderSocialPopup: function () {
@@ -83,48 +83,48 @@ var SocialMenu = {
         SocialMenu.opened = false;
     },
 
-    fbs_click: function(point) {
+    fbs_click: function (point) {
 
         var information = SocialMenu.getShareInformation(point);
-       window.open('http://www.facebook.com/sharer.php?u=' + encodeURIComponent(information[0]) + '&t=' + encodeURIComponent( information[1] ), 'sharer', 'toolbar=0,status=0,width=626,height=436');
+        window.open('http://www.facebook.com/sharer.php?u=' + encodeURIComponent(information[0]) + '&t=' + encodeURIComponent(information[1]), 'sharer', 'toolbar=0,status=0,width=626,height=436');
 
 
     },
-    google_click: function(point) {
-             var information = SocialMenu.getShareInformation(point);
+    google_click: function (point) {
+        var information = SocialMenu.getShareInformation(point);
         window.open('https://plus.google.com/share?url=' + encodeURIComponent(information[0]), 'sharer', 'toolbar=0,status=0,width=626,height=436');
     },
-    twt_click: function(point)
-    {
+    twt_click: function (point) {
         var information = SocialMenu.getShareInformation(point);
         window.open('http://twitter.com/share?url=' + encodeURIComponent(information[0]) + '&text=' + encodeURIComponent(information[1]), 'sharer', 'toolbar=0,status=0,width=626,height=436');
     },
 
-    getShareInformation:function(pointId)
-    {
+    getShareInformation: function (pointId) {
 
-            var selectedPoi =null;
-            var floor = -1
-           for(var selectedFloor in floors)
-           {
-                 selectedPoi=  floors[selectedFloor].pois.filter(function(item){return (item.id==pointId);});
-                 if(selectedPoi !=null)
-                 {
-                    floor = floors[selectedFloor].id;
-                     break;
-                 }
-           }
-            var enclosure = enclosure_id;
-            url = SocialMenu.urlstring+ enclosure +'_'+floor+'_'+pointId;
-         if(selectedPoi[0] && selectedPoi[0].description)
-         {
-              urlTitle =  'would you like to go to'+selectedPoi[0].description +'?';
-         }else
-         {
-              urlTitle =  'would you like to go to ?';
-         }
+        var selectedPoi = null;
+        var floor = -1
+        var description="";
+        if (poi_id == pointId) {
+            floor = floor_id;
+            description = qrPoint.point.description;
+        } else {
+            for (var selectedFloor in floors) {
+                for (var selectedPoi in floors[selectedFloor].pois) {
+
+
+                    if (floors[selectedFloor].pois[selectedPoi].id == pointId) {
+                        floor = floors[selectedFloor].id;
+                        description=floors[selectedFloor].pois[selectedPoi].description;
+                        break;
+                    }
+                }
+            }
+        }
+        var enclosure = enclosure_id;
+        url = SocialMenu.urlstring + enclosure + '_' + floor + '_' + pointId;
+        urlTitle = 'would you like to go to ' + description + '?';
         var result = new Array();
-        result[0]=url;
+        result[0] = url;
         result[1] = urlTitle;
         return result;
     }
