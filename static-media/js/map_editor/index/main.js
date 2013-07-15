8a877b90e6df2f124bb5a018bc6d4f6d2d15309a
rmajasol <rmajasol@gmail.com>
2013-07-15 17:17:41 +0200

    crud sobre plantas completado

--- static-media/js/map_editor/index/main.js
+++ static-media/js/map_editor/index/main.js
@@ -16,12 +16,15 @@ function EnclosureListCtrl($scope, $rootScope)
     // Orden en que aparecerán los recintos
     $scope.reverse = false;
 
-    $scope.show_create_form = function() {
-        $rootScope.$broadcast('show_create_form');
+    $scope.show_create_enclosure_form = function() {
+        $rootScope.$broadcast('show_create_enclosure_form');
     };
 
     angular.forEach(
-        ['enclosure_created', 'enclosure_updated', 'enclosure_deleted'],
+        [
+            'enclosure_created', 'enclosure_updated', 'enclosure_deleted',
+            'floor_created', 'floor_updated', 'floor_deleted'
+        ],
         function(event){
             $scope.$on(event, function() {
                 $scope.enclosures = enclosureResource.getForManagerIndex();
@@ -47,129 +50,28 @@ function EnclosureCtrl($scope, $rootScope, $element)
     };
 }
 
-function FloorListCtrl($scope, $element)
+function FloorListCtrl($scope, $rootScope)
 {
 	$scope.waiting_response = false;
     $scope.hovered = false;
+    $scope.reverse = false;
 
     $scope.$watch('floors', function(){
         FileInput.draw();
     });
 
-	$scope.createFloor = function() {
-
-        var img = $($element).find('input[name="img"]');
-        if(!img.val())
-        {
-            alert(gettext('You must specify the floor image too'));
-            return;
-        }
-		
-		//
-		// 1: Creamos el registro en B.D.
-		var floor_data = {
-			name : $scope.floor_name,
-			floor_number : parseInt($scope.floor_number),
-			enclosure : $scope.enclosure.resource_uri
-		};
-		
-		var new_floor = $scope.floor_resource.create(floor_data);
-		
-		//
-		// 2: Una vez creado subimos la imágen para el nuevo mapa creado	
-		var img_form = $($element).find('form').first();
-		
-		$scope.waiting_response = true;
-		
-		$scope.floor_resource.addImg(
-			img_form, 
-			new_floor.id,
-			function(server_response){
-				// Una vez se sube la imágen se limpia el formulario y se actualiza
-				// la lista de mapas para el lugar
-				$scope.floor_name = '';
-				$scope.floor_number = '';
-				img_form.find('input[name="img"]').val('');
-                img_form.find('.file-input-name').remove();
-				$scope.floors =
-					$scope.floor_resource.readAllFiltered('?enclosure__id=' + $scope.enclosure.id + '&order_by=floor_number');
-				
-				$scope.waiting_response = false;
-				
-				$scope.$apply();
-			}
-		);
-
-
-	};
-}
-
-function FloorCtrl($scope, $element)
-{
-	$scope.editing = false;
-
-
-	$scope.update = function() {
-		var img = $($element).find('input[name="img"]');
-
-		if (!$scope.editing)
-			$scope.editing = true;
-        else
-        {
-			// Si ya se estaba editando cuando hemos invocado update() entonces
-			// guardamos el nuevo nombre en la BD
-
-			var floor_data = {
-				name : $scope.floor_name,
-				floor_number : $scope.floor_number
-			};
-
-			$scope.floor_resource.update(floor_data, $scope.floor.id);
-			
-			// Si se ha puesto una nueva imágen la subimos, eliminando la anterior
-			if(img.val() !== '')
-			{					
-				var img_form = $($element).find('form');
-				
-				$scope.waiting_response = true;
-				
-				$scope.floor_resource.addImg(
-					img_form, 
-					$scope.floor.id,
-					function(server_response){
-						// Una vez se sube la imágen se limpia el formulario y se actualiza
-						// la lista de plantas para el recinto
-						$scope.waiting_response = false;
-					}
-				);
-			}
-
-			$scope.editing = false;
-            $scope.loadFloorList();
-		}
-
-        FileInput.draw();
-	};
-
-    $scope.cancelUpdate = function() {
-        $scope.editing = false;
-
-        $scope.loadFloorList();
+    $scope.show_create_floor_form = function() {
+        $rootScope.$broadcast('show_create_floor_form', $scope.enclosure);
     };
 
-	$scope.del = function() {
 
-		var confirm_msg = gettext('Are you sure you want to remove this floor?');
+}
 
-		$scope.floor_resource.del(
-            $scope.floor.id,
-            confirm_msg,
-            function(){
-                $($element).fadeOut(200);
-                setTimeout($scope.loadFloorList, 200);
-            }
-        );
-	};
+function FloorCtrl($scope, $rootScope, $element)
+{
+    $scope.show_edit_floor_form = function() {
+        $rootScope.$broadcast('show_edit_floor_form', $scope.floor);
+    }
 }
 
 
@@ -229,7 +131,7 @@ function EnclosureFormsCtrl($scope, $rootScope)
         );
     };
 
-    $scope.$on('show_create_form', function() {
+    $scope.$on('show_create_enclosure_form', function() {
         $scope.enclosure_name = '';
         formDialog = new FormDialog('#enc_create');
         formDialog.open();
@@ -245,9 +147,122 @@ function EnclosureFormsCtrl($scope, $rootScope)
     });
 }
 
-function FloorFormsCtrl($scope)
+function FloorFormsCtrl($scope, $rootScope, $element)
 {
+    $scope.create = function() {
+        var img = $($element).find('input[name="img"]');
+        if(!img.val())
+        {
+            alert(gettext('You must specify the floor image too'));
+            return;
+        }
 
+        //
+        // 1: Creamos el registro en B.D.
+        var floor_data = {
+            name : $scope.floor_name,
+            floor_number : parseInt($scope.floor_number),
+            enclosure : enclosureResource.api1_url + $scope.enclosure.id + '/'
+        };
+
+        var new_floor = floorResource.create(floor_data);
+
+        //
+        // 2: Una vez creado subimos la imágen para el nuevo mapa creado
+        var img_form = $($element).find('form').first();
+
+        $scope.waiting_response = true;
+
+        floorResource.addImg(
+            img_form,
+            new_floor.id,
+            function(server_response){
+                // Una vez se sube la imágen se limpia el formulario y se actualiza
+                // la lista de mapas para el lugar
+                $scope.floor_name = '';
+                $scope.floor_number = '';
+                img_form.find('input[name="img"]').val('');
+                img_form.find('.file-input-name').remove();
+                $scope.waiting_response = false;
+
+                // Anunciamos que se ha creado la planta
+                $rootScope.$broadcast('floor_created');
+            }
+        );
+    };
+
+
+    $scope.update = function() {
+        var img = $($element).find('input[name="img"]');
+
+        var floor_data = {
+            name : $scope.floor_name,
+            floor_number : $scope.floor_number
+        };
+
+        floorResource.update(floor_data, $scope.floor.id);
+
+        // Si se ha puesto una nueva imágen la subimos, eliminando la anterior
+        if(img.val() !== '')
+        {
+            var img_form = $($element).find('form');
+
+            $scope.waiting_response = true;
+
+            floorResource.addImg(
+                img_form,
+                $scope.floor.id,
+                function(server_response){
+                    // Una vez se sube la imágen se limpia el formulario y se actualiza
+                    // la lista de plantas para el recinto
+                    img_form.find('input[name="img"]').val('');
+                    img_form.find('.file-input-name').remove();
+                    $scope.waiting_response = false;
+                    $rootScope.$broadcast('floor_updated');
+                }
+            );
+        }
+        else
+            $rootScope.$broadcast('floor_updated');
+    };
+
+
+    $scope.cancelUpdate = function() {
+        formDialog.close();
+    };
+
+
+    $scope.del = function() {
+        var confirm_msg = gettext('Are you sure you want to remove this floor?');
+
+        floorResource.del(
+            $scope.floor.id,
+            confirm_msg,
+            function(){
+                $rootScope.$broadcast('floor_deleted');
+            }
+        );
+    };
+
+
+    $scope.$on('show_create_floor_form', function(ev, enclosure) {
+        $scope.enclosure = enclosure;
+        $scope.floor_name = '';
+        $scope.floor_number = '';
+        $scope.floor_img = '';
+        formDialog = new FormDialog('#floor_create');
+        formDialog.open();
+    });
+
+
+    $scope.$on('show_edit_floor_form', function(ev, floor) {
+        $scope.floor = floor;
+        $scope.floor_name = floor.name;
+        $scope.floor_number = floor.floor_number;
+        $scope.floor_img = floor.img;
+        formDialog = new FormDialog('#floor_edit');
+        formDialog.open();
+    });
 }
 
 
