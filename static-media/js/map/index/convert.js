c5ec48cfe76312985dedcdaf392a5d7f3ced0554
josemnopi <jose.m.duque@mnopi.com>
2013-07-22 17:59:27 +0200

    PRUEBA GEOJSON

---
+++ static-media/js/map/index/convert.js
@@ -0,0 +1,21 @@
+var mimapa = {
+    "type": "Feature",
+    "properties": {
+        "style": {
+            weight: 2,
+            color: "#999",
+            fillColor: "red"
+        }
+    },
+    "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+
+            [ 250.5, 405.5],
+            [ 306.5, 330.75 ],
+            [ 390.0, 391.0 ],
+            [ 250.5, 406.25],
+            [ 250.5, 405.5]
+        ]]
+    }
+}
