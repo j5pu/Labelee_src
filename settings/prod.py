105f8162dbec2cd90e92d6b1e79df1eb8d254d17
Alfredo <alfredo@alfredo.(none)>
2013-08-23 17:49:49 +0200

    Debug=false in production settings

---
+++ settings/prod.py
@@ -0,0 +1,36 @@
+"""Production settings and globals."""
+
+from common import *
+
+# Production database
+#########################################
+DATABASES = {
+    "default": {
+    "ENGINE": "django.db.backends.mysql",
+    "NAME": "labelee_dev",
+    "USER": "root",
+    "PASSWORD": "1aragon1",
+    "HOST": "localhost",
+    "PORT": "",
+    }
+}
+##########################################
+
+# DEBUG MODE IS OFF!!
+####################################
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+####################################
+
+
+# Only allowed hosts!!
+####################################
+ALLOWED_HOSTS = ['localhost:8000', '127.0.0.1', '192.168.1.33', '.compute.amazonaws.com']   #TODO: dejar el que toque!
+####################################
+
+
+#ADMINS = (
+#    ('Alfredo', 'alfredo.lainez@mnopi.com'),
+#)
+
+#MANAGERS = ADMINS
