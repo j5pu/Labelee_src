748627709abea8c79b7eb4ba841f60f29beaeb95
alfredolainez <alfredo.lainez@mnopi.com>
2013-09-03 15:55:27 +0200

    Beauty routes

--- route/calculateRoutes.py
+++ route/calculateRoutes.py
@@ -9,6 +9,7 @@ from map_editor.models import *
 from route.models import *
 from route.pathfinding.dijkstra import Dijkstra
 
+import math
 #import cProfile
 import time
 
@@ -37,7 +38,7 @@ class StepIterator:
 
     Attributes:
         single_steps : list of all points in a route
-        current_point : index of the current point in the list
+        current_point : index of the current point in the list of steps
     """
 
     def __init__(self, single_steps):
@@ -99,6 +100,172 @@ class StepIterator:
 
         # The next step is at the end of a line
         return self._findLineEnd()
+
+
+class BeautifyIterator:
+    """ Iterates over steps in a route interpolating shorter
+    lines if possible
+
+    For instance, given the following route
+
+                                    [4,2]    *    [6,2]
+
+                              *
+               ------>
+           [0,0]   *   [2,0]
+
+    the iterator would yield [0,0] and [6,2], given that there
+    are no walls in the points in which the segment [0,0],[6,2] lies
+
+    Attributes:
+        steps : list of steps in a route
+        walls : list of blocking points
+        current_point : index of the current point in the list of steps
+    """
+
+    SAMPLES_PER_BLOCK = 5
+
+    def __init__(self, steps, walls):
+        self.steps = steps
+
+        # Index walls by floor, column and row for quick look-up
+        self.walls = {}
+        for point in walls:
+            row, column, floor = [int(x) for x in point.split('_')]
+            if floor not in self.walls.keys():
+                self.walls[floor] = {}
+            if column not in self.walls[floor].keys():
+                self.walls[floor][column] = {}
+            self.walls[floor][column][row] = True
+
+    def __iter__(self):
+        self.current_point = None
+        return self
+
+    @staticmethod
+    def getCoordinatedPoint(point):
+        row, column, floor = point.split('_')
+        return {'x': int(column), 'y': int(row), 'floor': int(floor)}
+
+    @staticmethod
+    def getLineIncrements(point1, point2):
+        x_diff = float(point2['x'] - point1['x'])
+        y_diff = float(point2['y'] - point1['y'])
+        line_length = math.sqrt (x_diff * x_diff + y_diff * y_diff)
+
+        # The increment depends on a constant SAMPLES_PER_BLOCK > 0 to ensure that even when the line presence in a
+        # block is short, the block will be considered. Larger values of R assure accuracy but decrease
+        # performance
+        increments = {}
+        increments['x'] = (x_diff / line_length) / BeautifyIterator.SAMPLES_PER_BLOCK
+        increments['y'] = (y_diff / line_length) / BeautifyIterator.SAMPLES_PER_BLOCK
+
+        return increments
+
+    @staticmethod
+    def getBlock(x,y):
+        """ Detects in which block a coordinate point is.
+        A block comprises:
+                            x,y+0.5
+               x-0.5,y        x,y       x+0.5,y
+                            x, y-0.5
+        """
+        block = {}
+        if math.modf(x)[0] <= 0.5:
+            block['col'] = int(math.modf(x)[1])
+        else:
+            block['col'] = int(math.modf(x)[1] + 1)
+
+        if math.modf(y)[0] <= 0.5:
+            block['row'] = int(math.modf(y)[1])
+        else:
+            block['row'] = int(math.modf(y)[1] + 1)
+
+        return block
+
+    def _isLinePossible(self, origin_index, end_index):
+        """ Checks if a line is possible by looking for walls
+        """
+        point1 = self.getCoordinatedPoint(self.steps[origin_index])
+        point2 = self.getCoordinatedPoint(self.steps[end_index])
+
+        if point1['floor'] != point2['floor']:
+            return False
+
+        floor = point1['floor']
+        increments = self.getLineIncrements(point1, point2)
+
+        x = point1['x']
+        y = point1['y']
+        block = self.getBlock(x,y)
+        finalBlock = self.getBlock(point2['x'], point2['y'])
+        #print "Origin point" + str(origin_index)
+        #print "End point" + str(end_index)
+        #print "Final block" + str(finalBlock)
+        #print "WALLS" + str(self.walls)
+        #print increments['x']
+        #print increments['y']
+        while block != finalBlock:
+
+            #print block
+            #print self.walls.get(floor, False)
+            #print self.walls[floor].get(block['col'], False)
+            #print self.walls[floor][block['col']].get(block['row'], False)
+            # If a block passed by the line is a wall, the line is considered not possible
+            if self.walls.get(floor, False) and self.walls[floor].get(block['col'], False) \
+                and self.walls[floor][block['col']].get(block['row'], False):
+
+                return False
+
+            x += increments['x']
+            y += increments['y']
+            block = self.getBlock(x,y)
+
+        # All the blocks are clear
+
+        #print "SALGO"
+
+        return True
+
+    def _interpolate(self):
+        point1 = self.getCoordinatedPoint(self.steps[self.current_point])
+        point2 = self.getCoordinatedPoint(self.steps[self.current_point + 2]) # ignore direct next point
+
+        # A change of floor prohibits interpolation
+        if point1['floor'] != point2['floor']:
+            self.current_point += 1
+            return self.steps[self.current_point]
+        else:
+            initialPoint = self.current_point
+            self.current_point += 1
+
+            # Look for possible interpolations with all the following points
+            #print self.steps
+            for pointToTest in range(self.current_point + 1, len(self.steps)):
+                #print "ATACANDO RECTA AL PUNTO" + str(pointToTest)
+                if self._isLinePossible(initialPoint, pointToTest):
+                    self.current_point = pointToTest
+
+            # Return last (the longest) interpolation found, or the next point if none was found
+            return self.steps[self.current_point]
+
+    def next(self):
+        # The first step is the first point
+        if self.current_point == None:
+            #print "base"
+            self.current_point = 0
+            return self.steps[0]
+        elif self.current_point + 2 == len(self.steps):
+            #print "a punto de acabar"
+            self.current_point += 1
+            return self.steps[self.current_point]
+        elif self.current_point + 1 == len(self.steps):
+            #print "adios"
+            raise StopIteration
+
+        # Interpolate line for next step
+        return self._interpolate()
+
 
 
 def calculate_routes(request, enclosure_id):
@@ -196,7 +363,10 @@ def threadCalculateRoute(enclosure_id):
             routes = []
             steps = []
             with transaction.commit_on_success():
+                a = []
                 for path in paths:
+                    a.append(1)
+                    print len(a)
                     calculatedRoute = Route()
                     calculatedRoute.origin = path[0].point
                     calculatedRoute.destiny = path[1].point
@@ -204,7 +374,14 @@ def threadCalculateRoute(enclosure_id):
                     routes.append(calculatedRoute)
 
                     step_number = 0
+                    route = []
+
+                    # Eliminate intermediate steps
                     for step in StepIterator(path[2]):
+                        route.append(step)
+
+                    # Beautify routes by the means of interpolating lines
+                    for step in BeautifyIterator(route, walls):
                         step_number += 1
                         routeStep = Step()
                         routeStep.row, routeStep.column, routeStep.floor_id = step.split('_')
@@ -212,6 +389,7 @@ def threadCalculateRoute(enclosure_id):
                         routeStep.step_category = routeStep.NORMAL #TODO: esto se usa para algo?
                         routeStep.route = calculatedRoute
                         steps.append(routeStep)
+
 
                     # Limit the number of steps saved so they don't collapse main memory
                     if len(steps) > LIMIT_PER_BULK_SAVE:
