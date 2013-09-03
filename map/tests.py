434db55423accf44c63207728a4cc79325f6bf04
Alvaro Gutierrez Leña <alvaro.gutierrez@mnopi.com>
2013-09-03 11:29:36 +0200

    Testeando Jenkins y Selenium

--- map/tests.py
+++ map/tests.py
@@ -12,6 +12,9 @@ from django.test.testcases import LiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.wait import WebDriverWait
 import time
 
 
@@ -29,15 +32,35 @@ class SimpleTest(TestCase):
 
 class FunctionalTests(LiveServerTestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        """ Setup class needed by Selenium. It will setup the driver and assign live_server_url etc."""
+        cls.driver = WebDriver()
+        super(LiveServerTestCase, cls).setUpClass()
+
+    @classmethod
+    def tearDownClass(cls):
+        """ The teardown will make sure that you close the browser and shutdown the test server etc """
+        cls.driver.quit()
+        super(LiveServerTestCase, cls).tearDownClass()
+
+    def setUp(self):
+        """Here you initialize your test. Example if you need to login for testing, do it here"""
+        pass
+
+    def tearDown(self):
+        """Here you tear down after each test"""
+        pass
+
     def test_Selenium(self):
-        browser = webdriver.Firefox() # Get local session of firefox
-        browser.get("http://www.yahoo.com") # Load page
-        assert "Yahoo!" in browser.title
-        elem = browser.find_element_by_name("p") # Find the query box
+        # Get local session of firefox
+        self.driver.get("http://www.yahoo.com") # Load page
+        assert "Yahoo!" in self.driver.title
+        elem = self.driver.find_element_by_name("p") # Find the query box
         elem.send_keys("seleniumhq" + Keys.RETURN)
         time.sleep(0.2) # Let the page load, will be added to the API
         try:
-            browser.find_element_by_xpath("//a[contains(@href,'http://seleniumhq.org')]")
+            self.driver.find_element_by_xpath("//a[contains(@href,'http://seleniumhq.org')]")
         except NoSuchElementException:
             assert 0, "can't find seleniumhq"
-        browser.close()
+        self.driver.close()
