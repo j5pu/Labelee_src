5496cfb441cf4d3f176b37e5ecf7777a5f0cba80
Alvaro Gutierrez Leña <alvaro.gutierrez@mnopi.com>
2013-09-10 10:03:30 +0200

    Mejoras en la impresión de etiquetas

--- static-media/js/map_editor/index/main.js
+++ static-media/js/map_editor/index/main.js
@@ -45,6 +45,11 @@ function EnclosureCtrl($scope, $rootScope, $element)
         enclosureResource.refreshCache($scope.enclosure.id);
     };
 
+    $scope.qrDownload = function()
+    {
+        enclosureResource.qrDownload($scope.enclosure.id);
+    };
+
     $scope.$on('sync_enclosure', function(ev, enclosure) {
         if($scope.enclosure.id == enclosure.id)
         {
