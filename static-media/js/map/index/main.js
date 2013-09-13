e06d7961f087399bd4b2394d5a50674ea96d554f
Alvaro Gutierrez Leña <alvaro.gutierrez@mnopi.com>
2013-09-13 12:14:00 +0200

    Se ha añadido un mensaje de advertencia sobre el uso de cookies. Por ahora está desactivado.

--- static-media/js/map/index/main.js
+++ static-media/js/map/index/main.js
@@ -1,17 +1,17 @@
 var ua = navigator.userAgent;
-var androidversion = parseFloat(ua.slice(ua.indexOf("Android")+8));
+var androidversion = parseFloat(ua.slice(ua.indexOf("Android") + 8));
 
 var mySwiper;
 
 
 //PREFIX-FREE PLUG-INS
-(function($, self){
+(function ($, self) {
 
-    if(!$ || !self) {
+    if (!$ || !self) {
         return;
     }
 
-    for(var i=0; i<self.properties.length; i++) {
+    for (var i = 0; i < self.properties.length; i++) {
         var property = self.properties[i],
             camelCased = StyleFix.camelCase(property),
             PrefixCamelCased = self.prefixProperty(property, true);
@@ -20,41 +20,64 @@ var mySwiper;
     }
 
 })(window.jQuery, window.PrefixFree);
-(function(a){if(a)a.events={DOMNodeInserted:function(b){var b=b.target,c=b.nodeName;b.nodeType==1&&(/link/i.test(c)?a.link(b):/style/i.test(c)?a.styleElement(b):b.hasAttribute("style")&&a.styleAttribute(b))},DOMAttrModified:function(b){b.attrName==="style"&&(document.removeEventListener("DOMAttrModified",a.events.DOMAttrModified,false),a.styleAttribute(b.target),document.addEventListener("DOMAttrModified",a.events.DOMAttrModified,false))}},document.addEventListener("DOMContentLoaded",function(){document.addEventListener("DOMNodeInserted",
-    a.events.DOMNodeInserted,false);document.addEventListener("DOMAttrModified",a.events.DOMAttrModified,false)},false)})(window.StyleFix);
-(function(a){if(a&&window.CSSStyleDeclaration)for(var b=0;b<a.properties.length;b++){var c=StyleFix.camelCase(a.properties[b]),d=a.prefixProperty(c),e=CSSStyleDeclaration.prototype,f=function(a){return function(){return this[a]}}(d),d=function(a){return function(b){this[a]=b}}(d);Object.defineProperty?Object.defineProperty(e,c,{get:f,set:d,enumerable:true,configurable:true}):e.__defineGetter__&&(e.__defineGetter__(c,f),e.__defineSetter__(c,d))}})(window.PrefixFree);
-/*
-//Pantalla completa
-function hideAddressBar()
-{
-
-    if(( ua.indexOf("Android") >= 0 ) && (androidversion < 2.3))
-            return;
-
-    if(!window.location.hash)
-    {
-        if(document.height < window.outerHeight)
-        {
-            document.body.style.height = (window.outerHeight + 50) + 'px';
-        }
-
-        setTimeout( function(){ window.scrollTo(0, 1); }, 50 );
+(function (a) {
+    if (a)a.events = {DOMNodeInserted: function (b) {
+        var b = b.target, c = b.nodeName;
+        b.nodeType == 1 && (/link/i.test(c) ? a.link(b) : /style/i.test(c) ? a.styleElement(b) : b.hasAttribute("style") && a.styleAttribute(b))
+    }, DOMAttrModified: function (b) {
+        b.attrName === "style" && (document.removeEventListener("DOMAttrModified", a.events.DOMAttrModified, false), a.styleAttribute(b.target), document.addEventListener("DOMAttrModified", a.events.DOMAttrModified, false))
+    }}, document.addEventListener("DOMContentLoaded", function () {
+        document.addEventListener("DOMNodeInserted",
+            a.events.DOMNodeInserted, false);
+        document.addEventListener("DOMAttrModified", a.events.DOMAttrModified, false)
+    }, false)
+})(window.StyleFix);
+(function (a) {
+    if (a && window.CSSStyleDeclaration)for (var b = 0; b < a.properties.length; b++) {
+        var c = StyleFix.camelCase(a.properties[b]), d = a.prefixProperty(c), e = CSSStyleDeclaration.prototype, f = function (a) {
+            return function () {
+                return this[a]
+            }
+        }(d), d = function (a) {
+            return function (b) {
+                this[a] = b
+            }
+        }(d);
+        Object.defineProperty ? Object.defineProperty(e, c, {get: f, set: d, enumerable: true, configurable: true}) : e.__defineGetter__ && (e.__defineGetter__(c, f), e.__defineSetter__(c, d))
     }
-}
+})(window.PrefixFree);
+/*
+ //Pantalla completa
+ function hideAddressBar()
+ {
 
-window.addEventListener("load", function(){ if(!window.pageYOffset){ hideAddressBar(); } } );
-window.addEventListener("orientationchange", hideAddressBar );
-*/
+ if(( ua.indexOf("Android") >= 0 ) && (androidversion < 2.3))
+ return;
+
+ if(!window.location.hash)
+ {
+ if(document.height < window.outerHeight)
+ {
+ document.body.style.height = (window.outerHeight + 50) + 'px';
+ }
+
+ setTimeout( function(){ window.scrollTo(0, 1); }, 50 );
+ }
+ }
+
+ window.addEventListener("load", function(){ if(!window.pageYOffset){ hideAddressBar(); } } );
+ window.addEventListener("orientationchange", hideAddressBar );
+ */
 
 
 
 ///	Activación y configuración del menú
-$(function() {
+$(function () {
 
 
 //    if(androidversion <= 2.3)
 //    {
-        ScrollMenu.init();
+    ScrollMenu.init();
 //    }
     Panorama.init();
 
@@ -82,14 +105,14 @@ $(function() {
 
     $('#cupones, #header, span.locator, div#marquee').hide();
 
-   setTimeout(hideSplash, 100);
+    setTimeout(hideSplash, 100);
 
     $('div.swiper-slide img').on('click', function (e) {
         e.preventDefault();
         var cupPoint = parseInt($(this).prop('id')),
             cupFloor = new PointResource().read(cupPoint).floor,
             strL = cupFloor.length,
-            cupFloor = parseInt(cupFloor.substring(strL-3, strL-1));
+            cupFloor = parseInt(cupFloor.substring(strL - 3, strL - 1));
 
         drawRoute(qrPoint.point.id, cupPoint);
         $('div.device').fadeOut();
@@ -102,15 +125,13 @@ function hideSplash() {
     $('div#page').fadeIn(100);
     $('div.splash').fadeOut(100);
 
-    if(!localStorage.getItem('first_shoot'))
-    {
+    if (!localStorage.getItem('first_shoot')) {
         HelpMenu.show();
     }
 
     loadFloors();
 
-    if(qr_type == 'dest')
-    {
+    if (qr_type == 'dest') {
         $('#header, #cupones, #myCar').hide();
     }
 
@@ -118,10 +139,8 @@ function hideSplash() {
 }
 
 
-function showRouteFromMenu(origin_id, destination_id)
-{
-    if(origin_id != destination_id)
-    {
+function showRouteFromMenu(origin_id, destination_id) {
+    if (origin_id != destination_id) {
         drawRoute(origin_id, destination_id);
         LocalStorageHandler.setPrevDestByPoi(destination_id);
         $('#menu-right').trigger('close');
@@ -141,48 +160,40 @@ function showRouteFromMenu(origin_id, destination_id)
 var Coupon = {
     opened: false,
 
-    init: function()
-    {
+    init: function () {
         //Coupon.calculateCouponArea();
         Coupon.bindOpen();
     },
 
 
-    bindOpen: function()
-    {
+    bindOpen: function () {
         $('div#cupones').on('click', function (ev) {
             ev.stopPropagation();
-            if (!Coupon.opened)
-            {
+            if (!Coupon.opened) {
                 Coupon.open();
-            }else{
+            } else {
                 Coupon.close();
             }
         });
     },
 
 
-    bindShowFromMarker: function()
-    {
+    bindShowFromMarker: function () {
         $('div.leaflet-popup-content-wrapper').on('click', function (e) {
             //console.log(e.clientX +':'+ $(this).offset().left+':'+e.clientY +':'+ $(this).offset().top)
             if (e.clientX > $(this).offset().left + 135 &&
-                e.clientY > $(this).offset().top + 67)
+                e.clientY > $(this).offset().top + 67) {
+                var imgID = $(this).find('p>button').data('socialmenu'),
+                    myImg = "img[id='" + imgID + "']",
+                    myPos = $(myImg).parent()[0].index($(myImg).parent().parent());
 
-            {
-                var imgID=$(this).find('p>button').data('socialmenu'),
-                    myImg="img[id='"+imgID+"']",
-                    myPos=$(myImg).parent()[0].index($(myImg).parent().parent());
-
-           window.setTimeout(function(){
-                    mySwiper.swipeTo(myPos-1);
-                },500);
+                window.setTimeout(function () {
+                    mySwiper.swipeTo(myPos - 1);
+                }, 500);
 
 
-
-
-                 e.stopPropagation();
-                 Coupon.open();
+                e.stopPropagation();
+                Coupon.open();
 
             }
 
@@ -190,17 +201,15 @@ var Coupon = {
     },
 
 
-    open: function()
-    {
-        if(Panorama.opened) Panorama.close();
+    open: function () {
+        if (Panorama.opened) Panorama.close();
 
         $('div.device').fadeIn(100);
 
-        $(document).on('click tap touch', function(ev){
+        $(document).on('click tap touch', function (ev) {
             ev.stopPropagation();
-            if(Coupon.opened &&
-                ($('div.device').has($(ev.target)).length === 0 &&
-                    !$(ev.target).hasClass('device')))
+            if (Coupon.opened &&
+                ($('div.device').has($(ev.target)).length === 0 && !$(ev.target).hasClass('device')))
                 Coupon.close();
         });
 
@@ -209,22 +218,21 @@ var Coupon = {
     },
 
 
-/*    calculateCouponArea: function()
-    {
-        var $img = $('img#cupon-img');
+    /*    calculateCouponArea: function()
+     {
+     var $img = $('img#cupon-img');
 
-        var ancho = $img.width(),
-            alto = $img.height(),
-            vert = 360 / 469 * ancho,
-            imgCoords = "0," + alto + "," + ancho + "," + alto + "," + vert + ",0,0," + alto,
-            $area = $('div#cupones area');
+     var ancho = $img.width(),
+     alto = $img.height(),
+     vert = 360 / 469 * ancho,
+     imgCoords = "0," + alto + "," + ancho + "," + alto + "," + vert + ",0,0," + alto,
+     $area = $('div#cupones area');
 
-        $area.attr({'coords': imgCoords});
-    },*/
+     $area.attr({'coords': imgCoords});
+     },*/
 
 
-    close: function()
-    {
+    close: function () {
         Coupon.opened = false;
         $('div.device').fadeOut(100);
     }
@@ -233,8 +241,7 @@ var Coupon = {
 
 var ScrollMenu = {
 
-    init: function()
-    {
+    init: function () {
         this.$listMenu = $('#scrollMenu');
         this.$wrapper = $('nav');
 //        this.$wrapper = $('nav#menu-right');
@@ -246,8 +253,7 @@ var ScrollMenu = {
         this.scrollEvent();
     },
 
-    scroll: function(ev)
-    {
+    scroll: function (ev) {
         var self = ScrollMenu;
 
         ev.gesture.preventDefault();
@@ -258,18 +264,17 @@ var ScrollMenu = {
         });
     },
 
-    scrollEnd: function(ev)
-    {
+    scrollEnd: function (ev) {
         var self = ScrollMenu;
 
         ev.gesture.preventDefault();
 
         self.top_new = self.top + ev.gesture['deltaY'];
 
-        if(self.top_new > 50 || self.$listMenu.height() < self.$wrapper.height())
+        if (self.top_new > 50 || self.$listMenu.height() < self.$wrapper.height())
             self.top_new = 50;
-        else if(Math.abs(self.top_new) > self.$listMenu.height() - self.$wrapper.height()+25 )
-            self.top_new = self.$wrapper.height() - self.$listMenu.height()-25;
+        else if (Math.abs(self.top_new) > self.$listMenu.height() - self.$wrapper.height() + 25)
+            self.top_new = self.$wrapper.height() - self.$listMenu.height() - 25;
 
         self.$listMenu.css({
             'top': self.top_new + 'px'
@@ -278,8 +283,7 @@ var ScrollMenu = {
         self.top = self.top_new;
     },
 
-    scrollEvent: function()
-    {
+    scrollEvent: function () {
         var self = this;
 
         self.$listMenu.parent().hammer()
@@ -288,4 +292,14 @@ var ScrollMenu = {
     }
 };
 
+function showCookiesMessage() {
 
+    $.jqDialog.confirm("Usamos cookies para asegurarnos de que te ofrecemos la mejor experiencia posible en nuestro sitio web, para más información pulsa <a onclick='HelpMenu.showDisclaimer();'>aquí</a>. ¿Deseas activarlas? ",
+        function () {
+            alert("This intrusive alert says you clicked YES");
+        },		// callback function for 'YES' button
+        function () {
+            alert("This intrusive alert says you clicked NO");
+        }		// callback function for 'NO' button
+    );
+};
