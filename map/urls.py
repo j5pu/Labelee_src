43f654e20fd6d185ddacfb33b1d39a75e23e0dad
Alvaro Gutierrez Leña <alvaro.gutierrez@mnopi.com>
2013-11-19 18:32:51 +0100

    Borrar todo para hacer el merge con Labelee

--- map/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-
-urlpatterns = patterns('map.views',
-
-    url(r'^(?P<qr_type>.+)/((?P<enclosure_id>\d+)_(?P<floor_id>\d+)_(?P<poi_id>\d+))*$', 'show_map'),
-
-)
--- route/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from route.models import *
-
-admin.site.register(Route)
-admin.site.register(Step)
-admin.site.register(Connection)
